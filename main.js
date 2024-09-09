166a5857cacda1a1480801781564ca286de8c298
Elma1974 <elmaf@professor.educacao.sp.gov.br>
2024-09-08 22:33:53 -0300

    construção do main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+let nomeUsuário = prompt("Qual o seu nome?");
+let elemento = document.querySelector("#nome-usuário");
+
+while(nomeUsuário == ""){
+    nomeUsuário = prompt("Qual seu nome?");
+}
+
+if(nomeUsuário == null){
+    elemento.textContent = 'Seja muito bem-vindo';
+}else{
+elemento.textContent = nomeUsuário;
+}
+
